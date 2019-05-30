348a2e91bdacbb1e2e56cd3f44778d903bc7c0dc
liujie <1653259109@qq.com>
2019-05-30 22:21:18 +0800

    2

--- src/Test.java
+++ src/Test.java
@@ -6,8 +6,7 @@ public class Test {
 
 			System.out.println("hello world");
 			System.out.println("hello world01");
-			System.out.println("hello world02"); 
-			System.out.println("hello world03"); 
+			System.out.println("hello world02");
 	}
 
 }
