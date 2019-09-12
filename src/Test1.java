97641f15046bdd4c786f6e2fadcb0ab0540193ba
xiaokanchenfu86 <50519021+xiaokanchenfu86@users.noreply.github.com>
2019-09-12 15:03:09 +0800

    Update Test1.java

--- src/Test1.java
+++ src/Test1.java
@@ -3,13 +3,13 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- 		System.out.println("hello world2"); 
+ 		System.out.println("hello world1"); 
 	}
 	
 	
 	public static int add(int a,int b)
 	{
-			return a*b;
+			return a+b;
 	}
 
 }
