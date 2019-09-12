8f6bc75fd98a3e800882b92346f2587f15456693
liujie <1653259109@qq.com>
2019-09-12 17:50:02 +0800

    update

--- src/Test1.java
+++ src/Test1.java
@@ -3,21 +3,13 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
- 		System.out.println("hello world"); 
-=======
  		System.out.println("hello world1"); 
->>>>>>> branch 'master' of https://github.com/xiaokanchenfu86/TestGit.git
 	}
 	
 	
 	public static int add(int a,int b)
 	{
-<<<<<<< HEAD
-			return a-b;
-=======
 			return a+b;
->>>>>>> branch 'master' of https://github.com/xiaokanchenfu86/TestGit.git
 	}
 
 }
