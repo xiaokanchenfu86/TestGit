8ee243fbbf22cf736e4982f820f8ba461642a4bb
liujie <1653259109@qq.com>
2019-09-12 13:53:51 +0800

    update

--- src/Test.java
+++ src/Test.java
@@ -4,11 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-<<<<<<< HEAD
-			System.out.println("hello world");
-=======
-			System.out.println("hello world1");
->>>>>>> branch 'master' of https://github.com/xiaokanchenfu86/TestGit.git
+ 			System.out.println("hello world"); 
 	}
 
 }
